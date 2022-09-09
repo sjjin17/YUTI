3ecfd32fcc9302a2b948e0296f3885b3d5655c45
Minse5k <pms16011172@gmail.com>
2022-09-09 22:48:06 +0900

    feat: button 컴포넌트 제작

---
+++ frontend/atom/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { MAIN_COLOR } from '../const';
+
+const ButtonBase = styled.button`
+  background: white;
+  color: black;
+  border-radius: 10px;
+  border-color: rgb(0, 0, 0, 0.2);
+  height: 15vh;
+  box-sizing: border-box
+  &:hover {
+    background: ${MAIN_COLOR};
+    border: 0 solid;
+    color: white;
+  }
+  @media (max-width: 571px) {
+    width: 100%;
+    font-size: 4vw;
+  }
+  @media (min-width: 571px) {
+    width: 571px;
+    font-size: 22px;
+`;
+
+export default function Button({ text, onClick }) {
+  return (
+    <>
+      <ButtonBase onClick={onClick}>{text}</ButtonBase>
+    </>
+  );
+}
