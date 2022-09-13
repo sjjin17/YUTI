c0345dc7198c957f75b87c7d1ab2db2b7d9578a0
Minse5k <pms16011172@gmail.com>
2022-09-13 17:16:17 +0900

    feat: 메인페이지 작성

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IndexTemplate from '../template/IntdexTemplate';
 
 export default function Home() {
-  return <></>;
+  const [participantsNum, setParticipantsNum] = useState(0);
+  return <IndexTemplate participantsNum={participantsNum}></IndexTemplate>;
 }
