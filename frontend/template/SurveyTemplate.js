dbfc5a5d3c40dd792f96bd9d9e367e917044bb25
Minse5k <pms16011172@gmail.com>
2022-09-13 15:52:09 +0900

    style: layout 변경에 따른 SurveyTemplate css 변경

--- frontend/template/SurveyTemplate.js
+++ frontend/template/SurveyTemplate.js
@@ -7,11 +7,11 @@ import RangeBar from '../atom/RangeBar';
 import RangeNum from '../atom/RangeNum';
 
 const Container = styled.div`
-  width: 100%;
+  width: 80%;
   min-width: 100%;
-  height: 90%;
-  padding: 10px 20px 0px 20px;
-  margin: 0 auto;
+  height: 100%;
+  //padding: 10px 20px 0px 20px;
+  //margin: 0 auto;
   display: grid;
   justify-content: center;
   align-items: center;
