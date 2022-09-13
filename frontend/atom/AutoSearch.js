f81099512e11feee1351728e34c0c7cf55ff7706
이창엽 <john5755@naver.com>
2022-09-13 14:12:05 +0900

    [FEATURE] 검색 시 미리보기 창 #13 #S07P22A502-49

---
+++ frontend/atom/AutoSearch.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { MAIN_COLOR } from '../const';
+
+const AutoSearchContainer = styled.div`
+  z-index: 3;
+  width: 100%;
+  height: 50vh;
+  background-color: #fff;
+  border: 2px solid;
+  margin-top: 2.5%;
+  margin-bottom: 5%;
+`;
+
+const AutoSearchWrap = styled.ul``;
+
+const AutoSearchData = styled.li`
+  padding: 10px 8px;
+  width: 100%;
+  font-size: 14px;
+  font-weight: bold;
+  z-index: 4;
+  letter-spacing: 2px;
+  &:hover {
+    background-color: #edf5f5;
+    cursor: pointer;
+  }
+  position: relative;
+  img {
+    position: absolute;
+    right: 5px;
+    width: 18px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+
+export default function AutoSearch() {
+  return (
+    <AutoSearchContainer>
+      <AutoSearchWrap>
+        <AutoSearchData></AutoSearchData>
+      </AutoSearchWrap>
+    </AutoSearchContainer>
+  );
+}
