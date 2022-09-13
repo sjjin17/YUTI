fa979397da3d38d21e8f0fd6a893a48de51859fc
한지운 <hju0118@naver.com>
2022-09-13 14:09:50 +0900

    [FEATURE] 유튜버 이미지 컴포넌트 구현 #4 #S07P22A502-42

---
+++ frontend/pages/result.js
@@ -0,0 +1,11 @@
+import YoutuberImage from '../atom/YoutuberImage';
+import Medal from '../atom/Medal';
+
+function result() {
+  return (
+    <>
+      <YoutuberImage />
+    </>
+  );
+}
+export default result;
