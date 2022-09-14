e2a6fc68b2bdf8a2c320bbd8ac78f9dec8c5bd72
이창엽 <john5755@naver.com>
2022-09-14 14:07:46 +0900

    feat : 미리보기 및 선택입력

--- frontend/molecule/SelectedYtb.js
+++ frontend/molecule/SelectedYtb.js
@@ -1,8 +1,9 @@
-import styled from '@emotion/styled';
 import YoutuberImage from '../atom/YoutuberImage';
 
 export default function selectedYtb({ imageUrl }) {
-  <span>
-    <YoutuberImage imageUrl={imageUrl} youtuberName="name"></YoutuberImage>
-  </span>;
+  return (
+    <span>
+      <YoutuberImage imageUrl={imageUrl} youtuberName="name"></YoutuberImage>
+    </span>
+  );
 }
