7a4957d04318ca75a63273e6116771373f854d34
j1ddong <hju191228@gmail.com>
2022-09-14 13:03:07 +0900

    feat: 카카오 공유하기 컴포넌트 생성

---
+++ frontend/atom/KakaoShare.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const KakaoBtn = styled.img`
+  border-radius: 24px;
+  width: 45px;
+  height: 45px;
+  cursor: pointer;
+`;
+
+export default function KakaoShare() {
+  return <KakaoBtn src="images/kakaoLogo.png" alt="kakaoLogo" />;
+}
