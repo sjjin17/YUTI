b09b1e792181c45fd331c2111c9afb189f405210
노수빈 <qldtn8@naver.com>
2022-09-26 13:43:37 +0900

    [REFACTOR] 유튜버 검색 시 Youtube API  활용 #49 #S07P22A502-66

--- backend/main-server/src/main/java/com/yuti/mainserver/domain/youtuber/service/impl/YoutuberServiceImpl.java
+++ backend/main-server/src/main/java/com/yuti/mainserver/domain/youtuber/service/impl/YoutuberServiceImpl.java
@@ -1,15 +1,28 @@
 package com.yuti.mainserver.domain.youtuber.service.impl;
 
-import com.yuti.mainserver.domain.youtuber.service.YoutuberService;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.gson.Gson;
 import com.yuti.mainserver.domain.youtuber.dto.YoutuberResponseDto;
+import com.yuti.mainserver.domain.youtuber.service.YoutuberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -17,7 +30,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class YoutuberServiceImpl implements YoutuberService {
@@ -26,6 +41,16 @@ public class YoutuberServiceImpl implements YoutuberService {
 
     @Value("${es.index}")
     private String index;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${youtube.key}")
+    private String apiKey;
+
+    private static String PROPERTIES_FILENAME = "youtube.properties";
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    private static YouTube youtube;
 
     @Override
     public List<YoutuberResponseDto> searchYoutuber(String keyword, int offset) {
@@ -40,7 +65,10 @@ public class YoutuberServiceImpl implements YoutuberService {
         try{
             SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
 
-            return Arrays.stream(searchResponse.getHits().getHits())
+            if(searchResponse.getHits().getTotalHits().value == 0) {
+                return searchUsingYoutubeApi(keyword);
+            } else
+                return Arrays.stream(searchResponse.getHits().getHits())
                     .map(hit -> YoutuberResponseDto.toResponseDto(hit.getSourceAsMap()))
                     .collect(Collectors.toList());
 
@@ -48,4 +76,52 @@ public class YoutuberServiceImpl implements YoutuberService {
             throw new RuntimeException(e);
         }
     }
+
+    private List<YoutuberResponseDto> searchUsingYoutubeApi(String keyword) throws IOException {
+        youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+            public void initialize(HttpRequest request) throws IOException {}})
+                .setApplicationName("youtube-channel-search")
+                .build();
+
+        YouTube.Search.List search = youtube.search().list("snippet");
+        search.setKey(apiKey);
+        search.setQ(keyword);
+        search.setType("channel");
+
+        List<SearchResult> response = search.execute().getItems();
+        List<YoutuberResponseDto> searchYoutubers = response.stream()
+                .map(YoutuberResponseDto::toResponseDto)
+                .collect(Collectors.toList());
+
+        Gson gson = new Gson();
+        searchYoutubers.forEach(youtuber -> {
+            try {
+                SearchResponse status = restHighLevelClient.search(
+                        new SearchRequest(index).source(
+                                new SearchSourceBuilder().query(termQuery("channel_id", youtuber.getChannelId()))
+                        ), RequestOptions.DEFAULT);
+                if(status.getHits().getTotalHits().value == 0)
+                    sendDataToKafka("youtuber-analyzer", gson.toJson(youtuber));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return searchYoutubers;
+    }
+
+    private void sendDataToKafka(String topic, String data) {
+        kafkaTemplate.send(topic, data).addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error(ex.getMessage(), ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info(result.toString());
+            }
+        });
+    }
+
 }
