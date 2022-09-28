65148d9b756b4a0dd23165f9ea5e25afb1f5f855
Minse5k <pms16011172@gmail.com>
2022-09-28 10:47:49 +0900

    feat: App.js에 색상 및 context 설정

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,32 @@
 import '../styles/globals.css';
 import Layout from '../template/Layout';
+import { RouteChangeProvider } from '../context/RouteChangeContext';
+import { ThemeProvider } from '@emotion/react';
+import { useEffect, useCallback, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [colors, setColors] = useState();
+
+  useEffect(() => {
+    if (colors) {
+      return;
+    }
+    const [MAIN_COLOR, SUB_COLOR] =
+      Math.random() > 0.5 ? ['#67D193', '#2A9220'] : ['#FC7D71', '#FFC9CA'];
+    setColors({ main: MAIN_COLOR, sub: SUB_COLOR });
+  }, []);
+
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={{ colors }}>
+        <RouteChangeProvider>
+          {colors && (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
+        </RouteChangeProvider>
+      </ThemeProvider>
     </>
   );
 }
