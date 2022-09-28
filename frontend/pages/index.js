47543cf342c06e4fa3f94614311a4427ea075147
Minse5k <pms16011172@gmail.com>
2022-09-28 16:19:11 +0900

    feat: 메인페이지 SSG 설정

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,11 @@
 import { useTheme } from '@emotion/react';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouteContext } from '../context/RouteChangeContext';
 import IndexTemplate from '../template/IndexTemplate';
 import { sendTimeLog } from '../utils/log';
 import axios from '../utils/secondAxios';
 
-export default function Home() {
-  const [participantsNum, setParticipantsNum] = useState(0);
+export default function Home({ participantsNum }) {
   const { setSurveyNum, pageNumber } = useRouteContext();
   const theme = useTheme();
   useEffect(() => {
@@ -22,21 +21,31 @@ export default function Home() {
     });
   };
 
-  useEffect(() => {
-    getParticipantsNum();
-  }, [getParticipantsNum]);
-
-  const getParticipantsNum = useCallback(async () => {
-    try {
-      const res = await axios.get('/api/v1/mbti');
-      setParticipantsNum(res.data.data);
-    } catch (e) {}
-  }, []);
-
   return (
     <IndexTemplate
       participantsNum={participantsNum}
       handleSendLog={handleSendLog}
     ></IndexTemplate>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const res = await axios.get('/api/v1/mbti');
+    return {
+      props: { participantsNum: res.data.data },
+    };
+  } catch (error) {
+    if (error.response.status === 500) {
+      return {
+        redirect: {
+          destination: '/500',
+        },
+      };
+    } else {
+      return {
+        notFound: true,
+      };
+    }
+  }
 }
