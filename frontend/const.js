3039e7ee4bf6a5d8582ffbf3c956e41921e4b57b
박민석 <pms16011172@gmail.com>
2022-09-28 12:21:02 +0900

    [FEATURE] 페이지 이탈, 시간 로그 코드 작성 및 API 연결 & color A/B text 코드 변경 #78 #S07P22A502-127 #S07P22A502-128

--- frontend/const.js
+++
@@ -1,3 +0,0 @@
-const randomNum = Math.random();
-export const MAIN_COLOR = randomNum > 0.5 ? '#67D193' : '#FC7D71';
-export const SUB_COLOR = randomNum > 0.5 ? '#2A9220' : '#FFC9CA';
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import { useTheme } from '@emotion/react';
+import React, { useEffect, useState } from 'react';
+import { useRouteContext } from '../context/RouteChangeContext';
 import IndexTemplate from '../template/IndexTemplate';
+import { sendTimeLog } from '../utils/log';
 
 export default function Home() {
   const [participantsNum, setParticipantsNum] = useState(0);
-  return <IndexTemplate participantsNum={participantsNum}></IndexTemplate>;
+  const { setSurveyNum, pageNumber } = useRouteContext();
+  const theme = useTheme();
+  useEffect(() => {
+    setSurveyNum(0);
+  }, []);
+
+  const handleSendLog = () => {
+    sendTimeLog({
+      pageNo: pageNumber,
+      color: theme.colors.main === '#67D193' ? 'green' : 'red',
+      answer: '',
+      diffTime: new Date(),
+    });
+  };
+
+  return (
+    <IndexTemplate
+      participantsNum={participantsNum}
+      handleSendLog={handleSendLog}
+    ></IndexTemplate>
+  );
 }
