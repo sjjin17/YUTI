a77d4fb5da9b90990fb4fb4343e2b00124513594
chiyongs <tlscldyd@gmail.com>
2022-09-30 18:14:55 +0900

    [HOTFIX] 머지 충돌 해결

--- backend/main-server/src/main/java/com/yuti/mainserver/domain/youtuber/dto/YoutuberResponseDto.java
+++ backend/main-server/src/main/java/com/yuti/mainserver/domain/youtuber/dto/YoutuberResponseDto.java
@@ -1,41 +1,21 @@
 package com.yuti.mainserver.domain.youtuber.dto;
 
-import com.google.api.services.youtube.model.SearchResult;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
-import java.util.Map;
+import java.util.List;
 
-@ToString
 @Getter
 public class YoutuberResponseDto {
 
-    private String channelId;
-    private String channelName;
-    private String thumbnail;
+    private boolean isLast;
+    private String nextPageToken;
+    List<YoutuberDto> youtubers;
 
     @Builder
-    public YoutuberResponseDto(String channelId, String channelName, String thumbnail) {
-        this.channelId = channelId;
-        this.channelName = channelName;
-        this.thumbnail = thumbnail;
+    public YoutuberResponseDto(boolean isLast, String nextPageToken, List<YoutuberDto> youtubers) {
+        this.isLast = isLast;
+        this.nextPageToken = nextPageToken;
+        this.youtubers = youtubers;
     }
-
-    public static YoutuberResponseDto toResponseDto(Map<String, Object> response) {
-        return YoutuberResponseDto.builder()
-                .channelId((String) response.get("channel_id"))
-                .channelName((String) response.get("channel_name"))
-                .thumbnail((String) response.get("thumbnail"))
-                .build();
-    }
-
-    public static YoutuberResponseDto toResponseDto(SearchResult searchResult) {
-        return YoutuberResponseDto.builder()
-                .channelId(searchResult.getSnippet().getChannelId())
-                .channelName(searchResult.getSnippet().getChannelTitle())
-                .thumbnail(searchResult.getSnippet().getThumbnails().getHigh().getUrl())
-                .build();
-    }
-
 }
