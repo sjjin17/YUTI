fc4d00008aca8e149bd34109ed9338ef5b9e8066
한지운 <hju0118@naver.com>
2022-09-30 16:50:28 +0900

    [FEATURE] 결과페이지 환상/환장의 케미 구현

---
+++ frontend/molecule/OtherMbti.js
@@ -0,0 +1,47 @@
+import Heading from '../atom/Heading';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+import { MBTI_RESULT } from '../pages/[mbti]';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 165px;
+  cursor: pointer;
+  @media (min-width: 570px) {
+    width: 240px;
+  }
+`;
+
+const MbtiContainer = styled.div`
+  display: grid;
+  margin-top: 7px;
+  grid-template-rows: 0.7fr 1fr 4fr;
+  justify-items: center;
+  align-items: center;
+  box-shadow: 1px 1px 5px 0px #e3e3e3;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+export default function OtherMbti({ idx, mbti, onClick }) {
+  const mbtiImagePath = `/images/${mbti}.png`;
+  return (
+    <Container onClick={() => onClick(mbti)}>
+      <Heading
+        text={idx ? '환장의 케미' : '환상의 케미'}
+        fontSize="15px"
+        fontWeight="500"
+      />
+      <MbtiContainer>
+        <Heading text={mbti} fontSize="15px" highlighted />
+        <Heading
+          text={MBTI_RESULT[mbti].shareDesc}
+          fontSize="13px"
+          textAlign="center"
+        />
+        <Image src={mbtiImagePath} alt={mbti} width={150} height={150} />
+      </MbtiContainer>
+    </Container>
+  );
+}
