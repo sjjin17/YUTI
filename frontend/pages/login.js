362ad64682ef5a8e703e90d142f7a735dcfff354
Minse5k <pms16011172@gmail.com>
2022-10-02 13:59:03 +0900

    feat: 로그인 API 연동

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import LoginTemplate from '../template/LoginTemplate';
+import managerAxios from '../utils/managerAxios';
+import Router from 'next/router';
 
 export default function Login() {
   const [id, setId] = useState('');
@@ -10,10 +12,25 @@ export default function Login() {
   const handleChangePassword = e => {
     setPassword(e);
   };
+  const handleLoginSubmit = async () => {
+    try {
+      const credentials = {
+        id: id,
+        password: password,
+      };
+      const { data } = await managerAxios.post(
+        '/analytics/v1/accounts/login',
+        credentials,
+      );
+      localStorage.setItem('token', data.data);
+      Router.push({ pathname: '/manager' });
+    } catch {}
+  };
   return (
     <LoginTemplate
       handleChangeId={handleChangeId}
       handleChangePassword={handleChangePassword}
+      handleLoginSubmit={handleLoginSubmit}
     />
   );
 }
